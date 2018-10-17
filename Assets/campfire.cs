a475454087e5aa03c1364fb9e3f5830420daeaa9
MDS\darren.lit7358 <kitemax243@gmail.com>
2018-10-17 18:39:43 +1300

    completed campfire

--- Assets/campfire.cs
+++ Assets/campfire.cs
@@ -3,27 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class campfire : MonoBehaviour {
-    public GameObject[] listofenemys;
+    public GameObject listofenemys;
+    public EnemyMovement[] enemys;
+    public GameObject presseui;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        enemys = listofenemys.GetComponentsInChildren<EnemyMovement>();
+        presseui.SetActive(false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
         {
-
-            foreach (GameObject enemy in listofenemys)
-            {
-                enemy.GetComponent<EnemyMovement>().currHealth = 100;
-                enemy.SetActive(true);
+            presseui.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E)) {
+                other.GetComponent<Player>().CurrHealth = 100;
+                other.GetComponent<Player>().UpdateHealthBar();
+                foreach (EnemyMovement enemy in enemys)
+                {
+                    enemy.GetComponent<EnemyMovement>().currHealth = 100;
+                    enemy.gameObject.SetActive(true);
+                }
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            presseui.SetActive(false);
         }
     }
 }
